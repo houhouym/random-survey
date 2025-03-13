d8d7321970129613a7e3ac50d94f7d04676e819b
houhouym <ejje9119@gmail.com>
2025-03-14 00:49:17 +0800

    Update url.js

--- url.js
+++ url.js
@@ -9,6 +9,6 @@
 
 const urls = [
 	'https://www.tandfonline.com/toc/rina20/current',
-	'https://search.ebscohost.com/webauth/Community.aspx?code=eXelWsYFSau3&ref=SetToEmpty&ugt=723731563C6635573706352632853E6224E368D36613649362E326E334133603&authtype=ip&stsug=AgXAaPKU7CL0BNtsjYt0uYDX4wxI_IQwYblPWjcLqOcXDg5vCUxmcuJDYMpe24hutwq2P66vTPo8lZad8JKMR9XpJQVhGPpWYLl0Rxn5bv7U4PwRnLkkviJk7Vl26NRnhsJ9QTjZN1r2jcSCYVzXM_GbItxPJy1e4vVLUdCjRX6nx34&IsAdminMobile=N&encid=320771C67CC675C777C675C672C57CC734133803366334433263&selectServicesToken=AwW61wxMx9-4Nq_HHBNnhelABSb0Ovw1g_J_d6eZok3HtI-4TeOgyUKVfF7wG7YTnr8CmlzJWDBfp0gu-AjWWQ9NAW3OdelTMfiNfLlbQreis6k2m3xmrSW1irh-0sXX2MKf5meO-QeIK1VlHuwoyhdIb8VxHblwp8AhJQlC4NF466k40oqcTAApZnH1X8_sBU8kEOuWr6c-8Au7yV2kj_7bLGW0jeEPhMrVPW15Va8hodKjnNd7DVYi_zJoslcBN0co_1Va_RU1p9DmB6orZth_jWURhQ',
+	'https://docs.google.com/forms/d/e/1FAIpQLSfJmnGp7sSsn1kb_6KdDcydBomqWwM8IsrvcpVt6VBsZ5o97Q/closedform',
 	'https://www.youtube.com/watch?v=MYPVQccHhAQ',
 ];
